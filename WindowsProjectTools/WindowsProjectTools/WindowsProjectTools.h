dafac485fa83e2959b4a1f1757e2d6a4e3804492
ChrisWhatever1980 <Christian.Schlager@gmail.com>
2018-12-20 11:52:59 +0100

    Add files via upload

---
+++ WindowsProjectTools/WindowsProjectTools/WindowsProjectTools.h
@@ -0,0 +1,19 @@
+// WindowsProjectTools.h
+
+#pragma once
+
+#include <math.h>
+
+using namespace System;
+
+namespace WindowsProjectTools {
+
+	public ref class Class1
+	{
+	public:
+		double calcSqrt(double d)
+		{
+			return sqrt(d);
+		}
+	};
+}
